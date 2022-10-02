a5e26173e7de775e049fba73207799d7e32881a3
Aditya Patel <86345323+Charliehexx@users.noreply.github.com>
2022-10-02 23:40:13 +0530

    Update [id].js

--- pages/event/[id].js
+++ pages/event/[id].js
@@ -133,7 +133,7 @@ function Event({ event }) {
                     className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     onClick={newRSVP}
                   >
-                    RSVP for {ethers.utils.formatEther(event.deposit)} MATIC
+                    RSVP for {ethers.utils.formatEther(event.deposit)} GOERLI
                   </button>
                 )
               ) : (
